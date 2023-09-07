97f5fe122dfc812a012cb938b9f26a8e7ddc121d
nkubana0 <i.shema@alustudent.com>
2023-09-07 14:54:12 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main-print a string 
- * Description-use printf 
- * return 0
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
  */
-
 int main(void)
 {
-    printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-    return(0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
-
