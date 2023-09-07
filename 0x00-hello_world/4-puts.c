9bbdcb0ad60f68827782860ef5f8d6ca99df6384
nkubana0 <i.shema@alustudent.com>
2023-09-07 13:39:27 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	returns 0;
+	return (0);
 }	
