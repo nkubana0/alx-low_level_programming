4d420e9ed4c21ceca92aff74b89980086563937b
nkubana0 <i.shema@alustudent.com>
2023-09-09 11:11:40 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,20 +8,20 @@
  */
 
 int main(void){
-	int n;
+        int n;
 
-	srand(time(0));
-	n =rand() - RAND_MAX / 2;
-	if (n>0)
-	{
-		printf("%d is positive\n",n);
-	}
-        
+        srand(time(0));
+        n =rand() - RAND_MAX / 2;
+        if (n>0)
+        {
+                printf("%d is positive\n",n);
+        }
+
         else if (n==0){
-	        printf("%d is zero\n",n);
-	}
-	else{
-	        printf("%d is negative\n",n);
-	}
+                printf("%d is zero\n",n);
+        }
+        else{
+                printf("%d is negative\n",n);
+        }
 return 0;
 }
