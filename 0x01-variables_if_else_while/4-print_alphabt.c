827b2b16cfe156c3f33006493ad88c288a4ae660
nkubana0 <i.shema@alustudent.com>
2023-09-10 06:55:01 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-	char c=('a);
-	while(c<='z')
+	char c = 'a';
+	while (c <= 'z')
 	{
-		if(c!='q' && c!='e')
+		if(c != 'q' && c != 'e')
 		{
 			putchar(c);
-			c++;
+			
 		}
+		c++;
 		
 	}
 	putchar("\n");
