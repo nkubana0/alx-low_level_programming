396082ef825c4b6617d34b20aad31256c2fa5b22
nkubana0 <i.shema@alustudent.com>
2023-09-12 06:52:54 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point to my code
+ * description - Prints aplhabet
+ * returns 0
+ */
+int main(void)
+{
+	char c='a';
+	while(c<='z')
+	{
+		putchar(c);
+	}
+	c++;
+	return (0);
+}
