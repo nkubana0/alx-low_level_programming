7fbf3237d7d6a4d3dafb2206bcb4027640bf1e0d
nkubana0 <i.shema@alustudent.com>
2023-09-14 14:34:39 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print line - draws straight line in the terminal
+ * use _putchar 
+ * return : 0 always
+*/
+
+void print_line(int number)
+{
+    if (number > 0)
+    {
+        int count = 0;
+        
+        while (count < number)
+        {
+            _putchar(95);
+            count +=1;
+        }
+        _putchar('\n');
+        
+    }
+    else
+    {
+        _putchar('\n');
+    }
+}
