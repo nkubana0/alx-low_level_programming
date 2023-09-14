f36f0be4dd6caedb2e425974e7e50ea4e074fe50
nkubana0 <i.shema@alustudent.com>
2023-09-14 14:38:09 -0700

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,26 +2,25 @@
 
 /**
  * print line - draws straight line in the terminal
- * use _putchar 
+ * use _putchar
  * return : 0 always
 */
 
 void print_line(int number)
 {
-    if (number > 0)
-    {
-        int count = 0;
-        
-        while (count < number)
-        {
-            _putchar(95);
-            count +=1;
-        }
-        _putchar('\n');
-        
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (number > 0)
+	{
+		int count = 0;
+
+		while (count < number)
+		{
+			_putchar(95);
+			count += 1;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
