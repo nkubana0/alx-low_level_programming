d49ecbfc36e441615f0e3384a61bc41b569442f5
nkubana0 <i.shema@alustudent.com>
2023-09-14 14:19:55 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers 0-14 10 times
+ * use _putchar three times in code
+ * return : 0 always
+*/
+
+void more_numbers(void)
+{
+    int row,number;
+    for (row = 0; row < 10; row += 0)
+    {
+        number = 0;
+        while (number <=14)
+        {
+            if (number <= 10)
+            {
+                _putchar((number / 10) + '0');
+            }
+            _putchar((number % 10) + '0');
+            number += 1;
+        }
+        _putchar('\n');
+    }
+}
