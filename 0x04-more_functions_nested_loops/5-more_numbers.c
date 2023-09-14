41e9fc664e42cd997d9931f2fe373cb9e86f33fa
nkubana0 <i.shema@alustudent.com>
2023-09-14 14:23:13 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,21 @@
 
 void more_numbers(void)
 {
-    int row,number;
-    for (row = 0; row < 10; row += 0)
-    {
-        number = 0;
-        while (number <=14)
-        {
-            if (number <= 10)
-            {
-                _putchar((number / 10) + '0');
-            }
-            _putchar((number % 10) + '0');
-            number += 1;
-        }
-        _putchar('\n');
-    }
+	int row, number;
+
+	for (row = 0 ; row < 10 ; row += 1)
+	{
+		number = 0;
+
+		while (number <= 14)
+		{
+			if (number >= 10)
+			{
+				_putchar((number / 10) + '0');
+			}
+			_putchar((number % 10) + '0');
+			number += 1;
+		}
+		_putchar('\n');
+	}
 }
