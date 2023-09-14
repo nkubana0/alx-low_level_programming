a9b52d10573f60cb5c55352c903698a91a6262d8
nkubana0 <i.shema@alustudent.com>
2023-09-14 13:56:31 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_most_numbers(void) - prints numbers 0 - 9
+ * print_most_numbers - prints numbers 0 - 9
  * Do not print 2 and 4
  * Use _putchar only twice
  * Return 0
@@ -9,12 +9,13 @@
 
 void print_most_numbers(void);
 {
-	int number = '0';
-	while(number <='10')
+	int number = 0;
+
+	while (number < 10)
 	{
 		if (number !=2 && number != 4)
 		{
-			putchar(number + '0');
+			_putchar(number + '0');
 		}
 		number += 1;
 	}
