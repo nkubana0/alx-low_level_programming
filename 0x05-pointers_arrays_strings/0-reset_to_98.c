bca44b458b668114a9b1aa8b2f8bd48ad0ea62b4
nkubana0 <i.shema@alustudent.com>
2023-09-18 06:25:50 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change value of variable with pointer
+ * value 98
+ * @num: funtion parameter
+ * Return: pointer
+ */
+void reset_to_98(int *num)
+{
+	*num = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap 2 numbers
+ * num1: first parameter
+ * num2: second parameter
+ * Return: result
+ */
+
+void swap_int(int *num1, int *num2)
+{
+	int tmp;
+
+	tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
+}
