8f87cde37c70e4f9ac9a008368d1539d69bfe0c4
nkubana0 <i.shema@alustudent.com>
2023-09-20 22:35:27 -0700

    commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,10 +10,11 @@ int main(void)
   p = &n;
   /*
    * write your line of code here...
-   * not allowed to use a
-   * not allowed to modify p
-   * one statement
-   * not allowed to code anything else than this line of code
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
    */
   *(p + 5) = 98;
   /* ...so that this prints 98\n */
