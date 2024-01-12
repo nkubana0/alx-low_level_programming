14111047bdd18ce03d488b4ecbb829ee7f16b0e9
nkubana0 <i.shema@alustudent.com>
2024-01-12 11:58:38 -0800

    static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Ntsika Qumba");
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Shema Ivan");
     return (0);
 }
