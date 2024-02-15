41bf1385271f15aa5cd13c7cc14db9c90c711df7
nkubana0 <i.shema@alustudent.com>
2024-02-15 01:46:16 -0800

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd, bytes_read, bytes_written;
+    char *buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        free(buffer);
+        return (0);
+    }
+
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    free(buffer);
+    close(fd);
+    return (bytes_written);
+}
